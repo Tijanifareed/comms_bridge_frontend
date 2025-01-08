2feba9e1f631410648f531f533c37fa3e15cf014
~~Agent_freddie~~🔫 <fareedtijani2810@gmail.com>
2025-01-09 00:01:13 +0100

    home screen appbar and bottom navigation bar done remain the home body

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,16 +7,88 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final prefs = await SharedPreferences.getInstance();
-  final token = prefs.getString('authToken');
+  Future<String?> getUserName() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final userName = prefs.getString('userName');
+      print('Retrieved Username: $userName'); // Debugging log
+      return userName;
+    } catch (e) {
+      print('Error retrieving username: $e');
+      return null;
+    }
+  }
+
+  int _selectedIndex = 0;
+  final List<Widget> _pages = [
+    Center(child: Text('Home Page')),
+    Center(child: Text('Profile Page')),
+    Center(child: Text('Settings Page')),
+  ];
+
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueAccent,
       appBar: AppBar(
-        title: Text('Welcome to CommsBridge'),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.black,
+        title: FutureBuilder<String?>(
+            future: getUserName(),
+            builder: (context, snapshot){
+              if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text('Loading...'); // Show loading state
+              } else if (snapshot.hasError) {
+              return Text('Error'); // Handle error state
+              } else if (snapshot.hasData) {
+              return Text('Hi ${snapshot.data} Welcome Back!',
+                style: TextStyle(color: Colors.blueAccent, fontSize: 20),
+                textAlign: TextAlign.center,
+
+              );
+              } else {
+                return Text('Hi Guest',
+                  style: TextStyle(color: Colors.white),); // Default text if no username
+              }
+            }
+        ,
+        ),
       ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+                icon: Icon(Icons.home,
+                color: Colors.blueAccent,)
+            ),
+            BottomNavigationBarItem(
+              label: 'Profile',
+              icon: Icon(Icons.person,
+                color: Colors.blueAccent,
+              ),
+            ),
+            BottomNavigationBarItem(
+                label: 'Settings',
+
+                icon: Icon(Icons.settings,
+                color: Colors.blueAccent)
+            ),
+          ],
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.blueAccent,
+        currentIndex: _selectedIndex,
+        onTap: (index){
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+
+      ),
+
 
     );
   }
